a168e96d62b51eaac7a2ecea938a3360c8cd634d
shhio08 <kiiro18.sh@gmail.com>
2024-11-22 13:02:28 +0900

    wordpressもともとvolumeがある場合の処理追加 dbもともとvolumeがあるとエラー

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
-set -e
+# set -e
 
-# 環境変数の確認
-echo "MYSQL_DATABASE: $MYSQL_DATABASE"
-echo "MYSQL_USER: $MYSQL_USER"
-echo "MYSQL_PASSWORD: $MYSQL_PASSWORD"
-echo "MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD"
+
+# echo "installing mariadb-server..."
+# apt-get install -y mariadb-server
+
+echo "change bind-address..."
+sed -i 's/^bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# socketが見つからないというエラーでmkdirしてたけど、これのせいで立ち上がらなくなっていて（競合してます的な）これ消したら治った
+# mkdir  -p /run/mysqld/
+# adduser mysql
+# chown -R mysql:mysql /run/mysqld/
+
 
 mysqld_safe &
+
 
 until mysqladmin ping -h localhost --silent; do
   echo "Waiting for MariaDB to be ready..."
   sleep 2
 done
-touch flg
+
+
+
+# if ! mysqladmin ping -h localhost --silent; then
+#     echo "Error: MariaDB failed to start"
+#     exit 1
+# fi
+
+# service mariadb start
+# sleep 5
 
 echo "Running SQL script..."
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD') ;" > build.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" > build.sql
-echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;" >> build.sql  # バッククォートに修正
+# echo "SET PASSWORD FOR 'root'@'localhost'= PASSWORD('${MYSQL_ROOT_PASSWORD}') ;" > build.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" > build.sql
+echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;" >> build.sql
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> build.sql
-echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" >> build.sql  # バッククォートに修正
+echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';" >> build.sql
 echo "FLUSH PRIVILEGES ;" >> build.sql
 
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" < build.sql
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} < build.sql
 
-# MariaDB プロセスを前面で実行し続ける
+# #MariaDB プロセスを前面で実行し続ける
 wait
+# exec "$@"
--- srcs/requirements/wordpress/wordpress_init.sh
+++ srcs/requirements/wordpress/wordpress_init.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 cd /var/www/html
-wp config create --dbname=wordpress_db --dbuser=wordpress_user --dbpass=password --dbhost=db --allow-root
+if [ ! -f /var/www/html/index.php ]; then
+    wp core download --allow-root
+else
+    echo "WordPress already installed"
+fi
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp config create --dbname=wordpress_db --dbuser=wordpress_user --dbpass=password --dbhost=db --allow-root
+else
+    echo "wp-config.php already exists. Skipping config creation."
+fi
 wp core install --url=https://stakimot.42.fr --title=title --admin_user=admin --admin_password=password --admin_email=admin@test.com --allow-root
 
 exec php-fpm7.4 -F
